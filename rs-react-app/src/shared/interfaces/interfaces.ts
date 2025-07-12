9cc07afae4b80423cbea86b39ae531e792c7c305
Роман Бубнов <kilkun@icloud.com>
2025-07-12 21:10:08 +0400

    feat: add interfaces

---
+++ rs-react-app/src/shared/interfaces/interfaces.ts
@@ -0,0 +1,55 @@
+import { type ErrorInfo } from 'react';
+
+export interface ErrorBoundaryProps {
+  readonly children: React.ReactNode;
+  readonly onError?: (error: Error, errorInfo: ErrorInfo) => void;
+}
+
+export interface ErrorBoundaryState {
+  readonly hasError: boolean;
+  readonly error: Error | null;
+  readonly errorInfo: ErrorInfo | null;
+}
+
+type Sprites = {
+  [key: string]: string;
+};
+
+type TypeInfo = {
+  name: string;
+};
+
+type MyType = {
+  type: TypeInfo;
+};
+
+export interface PokemonType {
+  readonly sprites: Sprites;
+  readonly name: string;
+  readonly types: MyType[];
+  readonly height: string;
+  readonly weight: string;
+  readonly game_indices: [];
+}
+
+export interface MainPageState {
+  readonly query: string;
+  readonly data: PokemonType[];
+  readonly loading: boolean;
+  readonly error: null | Error;
+}
+
+export interface MainPageProps {
+  readonly query: string;
+  readonly data: PokemonType[];
+  readonly loading: boolean;
+  readonly error: null | Error;
+}
+
+export interface CardListType {
+  readonly data: PokemonType[];
+}
+
+export interface SearchInputType {
+  readonly onSearch: (searchValue: string) => void;
+}
