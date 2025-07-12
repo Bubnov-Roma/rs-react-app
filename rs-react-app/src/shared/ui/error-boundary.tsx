7b1f32968e65c223fcb373afa2c338b90ee0e25a
Роман Бубнов <kilkun@icloud.com>
2025-07-12 21:06:53 +0400

    feat: add errorboundary method

---
+++ rs-react-app/src/shared/ui/error-boundary.tsx
@@ -0,0 +1,35 @@
+import { Component, type ErrorInfo } from 'react';
+import type { ErrorBoundaryProps, ErrorBoundaryState } from '../interfaces';
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error, errorInfo: null };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    this.setState({ error, errorInfo });
+    if (this.props.onError) {
+      this.props.onError(error, errorInfo);
+    }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
